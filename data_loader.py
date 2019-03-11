3fefc29df4494b0e64a88d872787ad9319fdb2c1
ss1024fl <ss10240tw@gmail.com>
2019-03-11 17:52:40 +0800

    change data input

--- data_loader.py
+++ data_loader.py
@@ -7,106 +7,112 @@ from torch.utils.data import Dataset
 from torchvision import transforms
 
 from os.path import join as PJ
-from gensim.models import KeyedVectors
-import numpy as np
-
-import json
+import random
+import yaml
 
 
-class ClassDataset(Dataset):
+def ConceptSets(state, concepts):
 
-    def __init__(self, dataset='apy', mode='train_val', img_transform=None, label_transform=None):
-        self.mode = mode
-        self.dataset = dataset
-        self.root = PJ('./dataset', dataset)
+    split_list = state['split_list'] + ['general']
+    data_dir = PJ('./dataset', state['dataset'], 'list')
+    concept_file = pd.read_csv(PJ(data_dir, 'concepts', 'concepts_' + concepts + '.txt'))
 
-        self.img_root = PJ(self.root, 'img')
-        self.csv_file = PJ(self.root, 'list', mode + '.txt')
+    def _concept_split():
+        if state['mode'] == "train_val":
+            # load origin train_id
+            ids = yaml.load(open(PJ(data_dir, 'train_test', 'id_split' + '.txt')))
+            ids = ids['train_id']
 
-        self.data = pd.read_csv(self.csv_file, header=None)
-        self.img_transform = img_transform
-        self.label_transform = label_transform['concept_id']
+            # random generate id_split
+            val_nums = {'apy': 5, 'awa2': 13, 'cub': 50, 'sun': 65}
+            val_num = val_nums[state['dataset']]
+            random.shuffle(ids)
 
-    def __len__(self):
-        return len(self.data)
+            id_split = {'train_id': sorted(ids[val_num:]), 'test_id': sorted(ids[:val_num])}
+            print(id_split['train_id'])
+            print(id_split['test_id'])
 
-    def __getitem__(self, idx):
+            # produce split data file
+            data = pd.read_csv(PJ(data_dir, "train_val", "trainval.txt"), header=None)
 
-        image = Image.open(PJ(self.img_root, self.data.iloc[idx, 0])).convert('RGB')
-        label = torch.LongTensor([self.data.iloc[idx, 1]])
+            train_data = data[data.iloc[:, 1].isin(id_split['train_id'])]
+            train_data.to_csv(PJ(data_dir, "train_val", "train.txt"), index=False, header=False)
 
-        if self.img_transform:
+            test_data = data[data.iloc[:, 1].isin(id_split['test_id'])]
+            test_data.to_csv(PJ(data_dir, "train_val", "val.txt"), index=False, header=False)
+
+            return id_split
+        else:
+            return yaml.load(open(PJ(data_dir, state['mode'], 'id_split' + '.txt')))
+
+    def _concept(split_mode, concept_split):
+
+        if split_mode in ['train', 'trainval', 'test_seen']:
+            concept_label = concept_split['train_id']
+
+        elif split_mode in ['val', 'test_unseen']:
+            concept_label = concept_split['test_id']
+
+        elif split_mode in ['general']:
+            concept_label = list(range(concept_file.shape[1]))
+        else:
+            assert "Split Mode Error"
+
+        concept_vector = {i: torch.cuda.FloatTensor(concept_file.iloc[:, i].values) for i in concept_label}
+
+        id2label = {idx: label for idx, label in enumerate(concept_label)}
+
+        return {'concept_label': concept_label, 'concept_vector': concept_vector, 'id2label': id2label}
+
+    concept_split = _concept_split()
+    return {s: _concept(s, concept_split) for s in split_list}
+
+
+def ClassDatasets(state):
+
+    split_list = state['split_list']
+
+    class ClassDataset(Dataset):
+
+        def __init__(self, split_mode, state):
+
+            self.split_mode = split_mode
+            self.root = PJ('./dataset', state['dataset'])
+            self.csv_file = PJ(self.root, 'list', state['mode'], self.split_mode.strip("_g") + '.txt')
+
+            self.data = pd.read_csv(self.csv_file, header=None)
+            self.img_transform = self.img_transform()
+
+        def __len__(self):
+            return len(self.data)
+
+        def __getitem__(self, idx):
+
+            image = Image.open(PJ(self.root, 'img', self.data.iloc[idx, 0])).convert('RGB')
             image = self.img_transform(image)
 
-        if self.label_transform is not None:
-            label = torch.nonzero(self.label_transform == label).reshape(-1)
+            label = torch.LongTensor([self.data.iloc[idx, 1]])
 
-        sample = {'image': image, 'label': label}
-        return sample
+            sample = {'image': image, 'label': label}
+            return sample
 
+        def img_transform(self):
 
-class LabelSet():
+            if self.split_mode.find("train"):
+                img_transform = transforms.Compose([
+                    transforms.RandomResizedCrop(224),
+                    transforms.RandomHorizontalFlip(),
+                    transforms.ToTensor(),
+                    transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                         std=[0.229, 0.224, 0.225])])
+            else:
+                img_transform = transforms.Compose([
+                    transforms.Resize(256),
+                    transforms.CenterCrop(224),
+                    transforms.ToTensor(),
+                    transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                         std=[0.229, 0.224, 0.225])])
 
-    def __init__(self, dataset='apy', word2vec=None):
+            return img_transform
 
-        self.root = PJ('./dataset', dataset)
-        self.word2vec = word2vec
-
-        with open(PJ(self.root, 'list', 'id_split.txt')) as f:
-            self.id_split = json.load(f)
-        self.concepts = pd.read_csv(PJ(self.root, 'list', 'concepts.txt'), header=None).iloc[:, 0].values
-
-        self.train = self._concept('train_id')
-        self.test = self._concept('test_id')
-        self.all = self._concept(None)
-
-    def _concept(self, mode):
-
-        concept_id = torch.arange(len(self.concepts)) if mode is None else torch.LongTensor(self.id_split[mode])
-        concept_vectors = torch.FloatTensor([self.word2vec[self.concepts[i]].tolist() for i in concept_id])
-
-        return {'concept_id': concept_id, 'concept_vectors': concept_vectors}
-
-
-if __name__ == '__main__':
-
-    dataset = 'apy'
-    mode = ['train_val', 'test_seen', 'test_unseen']
-
-    # dataset test
-    train_transform = transforms.Compose([
-        transforms.RandomResizedCrop(224),
-        transforms.RandomHorizontalFlip(),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                             std=[0.229, 0.224, 0.225])])
-
-    test_transform = transforms.Compose([
-        transforms.Resize(256),
-        transforms.CenterCrop(224),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                             std=[0.229, 0.224, 0.225])])
-
-    # word2vec
-    weight_path = PJ('.', 'dataset', 'google_news', 'GoogleNews-vectors-gensim-normed.bin')
-    word2vec = KeyedVectors.load(weight_path, mmap='r')
-    word2vec.vectors_norm = word2vec.vectors
-
-    labelset = LabelSet('apy', word2vec)
-
-    trainval = ClassDataset(dataset='apy', mode='train_val',
-                            img_transform=train_transform,
-                            label_transform=labelset.train)
-
-    trainval_loader = torch.utils.data.DataLoader(trainval, batch_size=3, shuffle=False)
-
-    # show test data
-    for i, batch_data in enumerate(trainval_loader, 1):
-
-        images = batch_data['image']
-        labels = batch_data['label']
-
-    #     for img in images:
-    #         transforms.ToPILImage()(img).show()
-    #     break
+    return {s: ClassDataset(s, state) for s in split_list}
